2a36c2dd51a2f8f7a8b8b2f2ecc7fc7fe2bc3a28
Seyyed Hesam Mirebrahimi <hesaam.mir@gmail.com>
2021-04-27 15:43:42 +0430

    v1

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+
+date_default_timezone_set('Asia/Tehran');
+ini_set('memory_limit', '20480M');
+set_time_limit(0);
+
+
+use App\Kernel;
+use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\ErrorHandler\Debug;
+use Symfony\Component\HttpFoundation\Request;
+
+require getcwd() . '/core' . '/vendor/autoload.php';
+
+(new Dotenv())->bootEnv(getcwd() . '/core' . '/.env');
+
+if ($_SERVER['APP_DEBUG']) {
+    umask(0000);
+
+    Debug::enable();
+}
+
+if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? false) {
+    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
+}
+
+if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
+    Request::setTrustedHosts([$trustedHosts]);
+}
+
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool)$_SERVER['APP_DEBUG']);
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
